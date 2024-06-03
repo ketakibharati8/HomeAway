66aceacfe441f9c2868e74454c11f9a73297d07e
Ketaki Bharati <ketakibharati8@gmail.com>
2024-06-03 18:52:52 +0530

    Initial commit

---
+++ routes/listing.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const upload = multer({dest: "uploads/"});
+
+router
+.route("/")
+.get(wrapAsync(listingController.index))
+//.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+.post(upload.single('listing[image]'),(req,res) => {
+    res.send(req.file);
+});
+
+//New Route 
+router.get("/new",isLoggedIn, listingController.renderNewForm);
+
+
+router
+.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+.delete(isLoggedIn, isLoggedIn, wrapAsync(listingController.destroyListing) );
+
+
+//Edit Route 
+router.get("/:id/edit",isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+
+
+module.exports = router;
